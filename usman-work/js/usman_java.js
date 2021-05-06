30daf07773c86003cd4d23057ad50e1ed0916e65
usman yousaf <uyousaf709@gmail.com>
2021-05-06 19:40:18 +0500

    add course modal complete

--- usman-work/js/usman_java.js
+++ usman-work/js/usman_java.js
@@ -118,8 +118,10 @@ jQuery(function($) {
 
 // Activity Modal
 
-$('#video-d').hover(function() { // hover in
-    $('#video-d h6').css('color', '#fff');
+//Activity Modal Hover
+
+$('#video_course-d').hover(function() { // hover in
+    $('#video_course-d h6').css('color', '#fff');
     $('#widget-img').attr("src", "assets/preview/shopping-icon.svg");
 }, function() { // hover out
     $('#video_card-d h6').css('color', '#C3C3C3');
@@ -130,13 +132,43 @@ $('#video-d').hover(function() { // hover in
 
 $('#online_course-d').hover(function() { // hover in
     $('#online_course-d h6').css('color', '#fff');
-    // $('#online_course_img-d').css('color', '#fff');
-    $('#online_course_img-d').attr("src", "assets/preview/video-player.svg").css('color', '#fff');
+    $('#online_course_img-d').attr("src", "assets/preview/video-player.svg");
 }, function() { // hover out
     $('#course-d h6').css('color', '#C3C3C3');
 
     $('#online_course_img-d').attr("src", "assets/preview/video-icon.svg");
 });
+// $('#video_course-d').on('click', function() {
+//     $('#video_card-d').css('background-color', '#70B547')
+//     $('#video_course-d h6').css('color', '#fff');
+//     $('#widget-img').attr("src", "assets/preview/shopping-icon.svg");
+// });
+
+// Activity Type Modal Course Nature 
+
+$('#video_course-d').on('click', function(e) {
+    $('#hdn_course_nature_selection-d').val('video').attr('value', 'video');
+});
+
+$('#online_course-d').on('click', function(e) {
+    $('#hdn_course_nature_selection-d').val('online').attr('value', 'online');
+});
+
+$('.custom_button-d').on('click', function(e) {
+    let selectedCourseNature = $('#hdn_course_nature_selection-d').val();
+    if (selectedCourseNature == 'video') {
+
+        $('#activity_type_modal-d').modal('hide')
+        $('#video_course_details_modal-d').modal('show');
+    } else if (selectedCourseNature == 'online') {
+        $('#activity_type_modal-d').modal('hide')
+        $('#course_details_modal-d').modal('show');
+    } else {
+        $('#activity_type_modal-d').modal('show');
+    }
+});
+
+
 
 // Activity Modal End
 
