9368e03dd6a3fefca7082ae64d74f369244eafb6
Ahmed Nawaz Butt <ahmed.n@kodextech.com>
2021-05-06 12:22:18 +0500

    working on experience API

--- website/Modules/User/Database/Migrations/2021_05_06_071651_create_table_experiences.php
+++ website/Modules/User/Database/Migrations/2021_05_06_071651_create_table_experiences.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableEducations extends Migration
+class CreateTableExperiences extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateTableEducations extends Migration
      */
     public function up()
     {
-        Schema::create('educations', function (Blueprint $table) {
+        Schema::create('experiences', function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('uuid')->unique();
 
             $table->integer('profile_id')->unsigned();
 
-            $table->string('title');
-            $table->date('completed_at')->nullable();
-            $table->string('university');
+            $table->string('job_exp');
+            $table->string('teaching_exp');
             $table->string('image')->nullable();
 
             $table->index('profile_id');
@@ -39,6 +38,6 @@ class CreateTableEducations extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('educations');
+        Schema::dropIfExists('experiences');
     }
 }
