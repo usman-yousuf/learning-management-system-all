70b9f9d15caed68061855fbe26029011f5e613fc
shams-farooq <shams.kodextech@gmail.com>
2021-05-06 02:22:39 -0700

    workin on education api

---
+++ website/Modules/User/Entities/Education.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Modules\User\Entities;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class Education extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = "educations";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'title',
+        'completed_at',
+        'university',
+        'image',
+        'created_at',
+        'updated_at',
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'created_at' => 'date'
+    ];
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class, 'profile_id', 'id')->with('user');
+    }
+}
---
+++ website/Modules/User/Http/Controllers/API/EducationController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Modules\User\Http\Controllers\API;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Modules\Common\Services\CommonService;
+use Modules\User\Services\AddressService;
+use Modules\User\Services\EducationService;
+use Modules\User\Services\ProfileService;
+
+class EducationController extends Controller
+{
+    private $commonService;
+    private $addressService;
+    private $profileService;
+    private $educationService;
+
+    public function __construct(CommonService $commonService, AddressService $addressService, ProfileService $profileService ,EducationService $educationService)
+    {
+        $this->commonService = $commonService;
+        $this->addressService = $addressService;
+        $this->profileService = $profileService;
+        $this->educationService = $educationService;
+    }
+
+    /**
+     * Get a Single Address based on uuid
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getEducation(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'education_uuid' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $data['validation_error'] = $validator->getMessageBag();
+            return $this->commonService->getValidationErrorResponse($validator->errors()->all()[0], $data);
+        }
+        //  dd($request->all());
+        // validate and fetch an Education
+        $result = $this->educationService->checkEducation($request);
+        if(!$result['status']){
+            return $this->commonService->getProcessingErrorResponse($result['message'], [], 404, 404);
+        }
+        $education = $result['data'];
+
+        return $this->commonService->getSuccessResponse('Success', $education);
+    }
+
+    /**
+     * Delete an Education by UUID
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function deleteEducation(Request $request)
+    {   
+        $validator = Validator::make($request->all(), [
+            'education_uuid' => 'required|exists:educations,uuid',
+        ]);
+        if ($validator->fails()) {
+            $data['validation_error'] = $validator->getMessageBag();
+            return $this->commonService->getValidationErrorResponse($validator->errors()->all()[0], $data);
+        }
+
+        // validate and delete an Education
+        $result = $this->educationService->deleteEducation($request);
+        if (!$result['status']) {
+            return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+        }
+        $education = $result['data'];
+
+        return $this->commonService->getSuccessResponse('Record Deleted Successfully', []);
+    }
+
+    /**
+     * Get Addresses based on given filters
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getAddresses(Request $request)
+    {
+        if(isset($request->profile_uuid) && ('' != $request->profile_uuid)){
+            $result = $this->profileService->getProfile($request);
+            if (!$result['status']) {
+                return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+            }
+            $profile = $result['data'];
+            $request->merge(['profile_id' => $profile->id]);
+        }
+
+        $result = $this->addressService->getAddresses($request);
+        if (!$result['status']) {
+            return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+        }
+        $addresses = $result['data'];
+
+        return $this->commonService->getSuccessResponse('Success', $addresses);
+    }
+
+    /**
+     * Add|Update an Address
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function updateAddress(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'address_uuid' => 'exists:addresses,uuid',
+            'is_default' => 'required|in:0,1',
+            'profile_uuid' => 'exists:profiles,uuid',
+            'title' => 'string',
+            'address1' => 'required|min:5',
+            'address2' => 'string',
+            'city' => 'required|string',
+            'state' => 'string',
+            'country' => 'required|string',
+            'postal_code' => 'required',
+            // 'lat' => '',
+            // 'lng' => '',
+        ]);
+        if ($validator->fails()) {
+            $data['validation_error'] = $validator->getMessageBag();
+            return $this->commonService->getValidationErrorResponse($validator->errors()->all()[0], $data);
+        }
+
+        // get/set profile uuid
+        $uuid = $request->user()->profile->uuid;
+        if(isset($request->profile_uuid) && ('' != $request->profile_uuid)){
+            $uuid = $request->profile_uuid;
+        }
+        $request->merge(['profile_uuid' => $uuid]);
+
+        // valiadate and fetch profile
+        $result = $this->profileService->getProfile($request);
+        if (!$result['status']) {
+            return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+        }
+        $profile = $result['data'];
+        $request->merge(['profile_id' => $profile->id]);
+
+        // find address by uuid if given
+        $address_id = null;
+        if(isset($request->address_uuid) && ('' != $request->address_uuid)){
+            $result = $this->addressService->checkAddress($request);
+            if (!$result['status']) {
+                return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+            }
+            $address = $result['data'];
+            $address_id = $address->id;
+        }
+
+        $result = $this->addressService->addUpdateAddress($request, $address_id);
+        if (!$result['status']) {
+            return $this->commonService->getProcessingErrorResponse($result['message'], $result['data'], $result['responseCode'], $result['exceptionCode']);
+        }
+        $address = $result['data'];
+
+        return $this->commonService->getSuccessResponse('Success', $address);
+    }
+}
