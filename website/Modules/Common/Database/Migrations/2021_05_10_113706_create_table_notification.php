9b4a39cdb1a58a91ba3e6319e42eb804e94ae6b8
Ahmed Nawaz Butt <ahmed.n@kodextech.com>
2021-05-10 16:39:22 +0500

    added migration, model and service against notification

---
+++ website/Modules/Common/Database/Migrations/2021_05_10_113706_create_table_notification.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableNotification extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id')->unsigned(false);
+
+            $table->integer('sender_id')->unsigned();
+            $table->index('sender_id');
+            $table->foreign('sender_id')->references('id')->on('profiles')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('receiver_id')->unsigned();
+            $table->index('receiver_id');
+            $table->foreign('receiver_id')->references('id')->on('profiles')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('model_id')->comment('notification triggering model ID');
+            $table->string('noti_model')->comment('notification triggering model');
+            $table->string('noti_type')->comment('notification triggering key');
+            $table->text('noti_text')->comment('Notification Text');
+            $table->boolean('is_read')->default(false);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
