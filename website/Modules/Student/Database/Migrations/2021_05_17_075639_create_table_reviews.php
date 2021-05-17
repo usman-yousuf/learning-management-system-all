6c1843fa2e1faf58ca42030d8d376826cdd3247f
Ahmed Nawaz Butt <ahmed.n@kodextech.com>
2021-05-17 14:44:06 +0500

    working on teacher module

---
+++ website/Modules/Student/Database/Migrations/2021_05_17_075639_create_table_reviews.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('uuid')->unique();
+            $table->integer('course_id')->unsigned();
+
+            $table->integer('student_id')->unsigned();
+
+            $table->enum('star_rating', ['1', '2', '3', '4', '5'])->default('5');
+            $table->string('body')->nullable();
+
+            $table->index('course_id');
+            $table->foreign('course_id')->references('id')->on('courses')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index('student_id');
+            $table->foreign('student_id')->references('id')->on('profiles')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('');
+    }
+}
