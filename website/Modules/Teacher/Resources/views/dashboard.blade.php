54f758f1ad243135f10dfb2cbfeab39a6d8f3ff4
Ahmed Nawaz Butt <ahmed.n@kodextech.com>
2021-05-17 12:54:59 +0500

    working on teacher UI

---
+++ website/Modules/Teacher/Resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('teacher::layouts.teacher')
+
+@section('content')
+    <h1>Dashboard</h1>
+
+    <p>
+        This view is loaded from module: {!! config('teacher.name') !!}
+    </p>
+@endsection
