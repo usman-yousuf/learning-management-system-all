b52e5a76f7a1e4f9f89aba854234154de3ffbaaf
shams-farooq <shams.kodextech@gmail.com>
2021-06-03 10:47:23 +0500

    =working on notification Page

--- website/Modules/Common/Http/Controllers/NotificationController.php
+++ website/Modules/Common/Http/Controllers/NotificationController.php
@@ -5,11 +5,33 @@ namespace Modules\Common\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Common\Http\Controllers\API\NotificationsController;
+use Modules\Common\Services\CommonService;
 
 class NotificationController extends Controller
 {
+    private $commonService;
+    private $notificationCtrlObj;
+
+    public function __construct(
+        CommonService $commonService,
+        NotificationsController $notificationCtrlObj
+    ) {
+        $this->commonService = $commonService;
+        $this->notificationCtrlObj = $notificationCtrlObj;
+    }
+
     public function index(Request $request)
     {
-        return view('common::index');
+        $notifiCtrlObj = $this->notificationCtrlObj;
+
+        $apiResponse = $notifiCtrlObj->getNotificationsProfile($request)->getData();
+        dd($apiResponse);
+
+        if($request->getMethod()== 'GET')
+        {
+           
+        }
+        return view('common::notifications');
     }
 }
--- website/Modules/Common/Resources/views/notifications.blade.php
+++ website/Modules/Common/Resources/views/notifications.blade.php
@@ -1,11 +1,53 @@
 @extends('teacher::layouts.teacher')
 
 @section('page-title')
-    Sales Report
+    Notifications
 @endsection
 
 @section('content')
-
+    <div class="container-fluid px-5">
+        <div class="row pt-4">
+            <div class="col">
+                <h4 class="font_w_700-s">Notifications</h4>
+            </div>
+        </div>
+        <div class="row pt-5 pl-2">
+            <div class="col-12 notification_listing_container-d">
+                <div class="row single_notification-s px-2 py-4 pr-2 align-items-center">
+                    <!--notification image-->
+                    <div class="col-xl-1 col-lg-1 col-md-2 col-sm-1 col-3  text-center">
+                        <img class="notification_img-s img_w_50px-s" src="assets/preview/student4.png" alt="student img">
+                    </div>
+                    <!--notification image end-->
+                    <!--notification text-->
+                    <div class="col-xl-10 col-lg-10 col-md-8 col-sm-10 col-7  text-break text-wrap">
+                        <strong ><a class="no_link-s" href="javascript:void(0)">Amelia</a></strong>
+                        <br>
+                        <span>Enrolled into Graphic designing</span>
+                    </div>
+                    <!--notification text end-->
+                    <!--notification dropdown button-->
+                    <div class="col-xl-1 col-lg-1 col-md-2 col-sm-1 col-2">
+                        <div class="custom_dropdown-s  text-center">
+                            <img class="dropbtn" src="assets/preview/grey_dropdown_button.svg" alt="button img">
+                            <div class="custom_dropdown_content-s shadow bg-white text-left ">
+                                <div class="py-2 pl-2">
+                                    <a class="no_link-s" href="#">Link 1</a>
+                                </div>
+                                <div class="py-2 pl-2">
+                                    <a class="no_link-s" href="#">Link 2</a>
+                                </div>
+                                <div class="py-2 pl-2">
+                                    <a class="no_link-s" href="#">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--notification dropdown button-->
+                </div>
+            </div>                  
+        </div>
+    </div>
 @endsection
 
 
