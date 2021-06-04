fc524e7c0693a2a1edf048fc24f0381372a7b05e
shams-farooq <shams.kodextech@gmail.com>
2021-06-04 13:09:40 +0500

    commit Quiz index page

--- website/Modules/Quiz/Resources/views/quizez/index.blade.php
+++ website/Modules/Quiz/Resources/views/quizez/index.blade.php
@@ -1,167 +1,122 @@
 @extends('teacher::layouts.teacher')
 
 @section('page-title')
-    Quizez
+    Quizzez
 @endsection
 
 @section('content')
-    <div class="container-fluid px-5">
-        <div class="row py-4">
-            <div class="col-xl-6 col-lg-6 col-md-4 col-sm-12 col-12 align-self-center ">
-                <h3 class="top_courses_text-s mx-3 ">Quiz</h3>
-            </div>
-            <div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 col-12 ">
-                <div class="float-md-right ">
-                    <a href=" " class="btn btn py-3 px-5 add_course_btn-s " data-toggle="modal " data-target="#courses_outline_modal-d">
-                        <img src="assets/preview/add_button.svg " width="20 " id="add_outline_btn-d " class="mx-2 " alt=" ">
-                        <span class="mx-1 text-white ">Add Test</span>
-                    </a>
-                </div>
+<div class="container-fluid px-5">
+    <div class="row py-4">
+        <div class="col-xl-6 col-lg-6 col-md-4 col-sm-12 col-12 align-self-center ">
+            <h3 class="top_courses_text-s mx-3 ">Quiz</h3>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 col-12 ">
+            <div class="float-md-right ">
+                <a href=" " class="btn btn py-3 px-5 add_course_btn-s " data-toggle="modal " data-target="#courses_outline_modal-d">
+                    <img src="assets/preview/add_button.svg " width="20 " id="add_outline_btn-d " class="mx-2 " alt=" ">
+                    <span class="mx-1 text-white ">Add Test</span>
+                </a>
             </div>
         </div>
+    </div>
 
-        <!-- Quiz list - START -->
-        <div class="row pb-4">
-            {{-- {{ $data->quizzez }} --}}
-            @forelse ($data->quizzes as $item)
-                   <!-- List Item 1 - START --> 
-                @if ($item->type == 'mcqs')
-                    <div class="col-12 my-2 bg_white-s br_10px-s">
-                        <div class="row py-3">
-                            <div class="col-xl-6 col-lg-6 col-md-12 col-12">
-                                <h5 class="fg-success-s">Landing Page Design</h5>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
-                                <h6>Type: Test <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> 30 Minutes</h6>
-                            </div>
+    <!-- Quiz list - START -->
+    <div class="row pb-4">
+        <!-- List Item 1 - START -->
+        @forelse ($data->quizzes as $key => $item)
+            @if ($item->type == 'mcqs')
+                <div class="col-12 my-2 bg_white-s br_10px-s">
+                    <div class="row py-3">
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                            <h5 class="fg-success-s">{{ $item->course->title }}</h5>
                         </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
+                            <h6>Type: {{ strtoupper($item->type) }} <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> {{ $item->duration_mins }} Minutes</h6>
+                        </div>
+                    </div>
+                    @foreach ($item->questions as $question)
                         <div class="row">
                             <div class="col-11 fg_dark-s">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s?</p>
+                                <p>{{ $question->body }}</p>
+                                @break;
                             </div>
                         </div>
-                        <div class="row py-3">
-                            <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
-                                <span>Tottal Students: 40</span>
-                            </div>
-                            <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
-                                <span>Attending Test Student: 40</span>
-                            </div>
+                    @endforeach
+                    <div class="row py-3">
+                        <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
+                            <span>Tottal Students: {{ $item->students_count }}</span>
+                        </div>
+                        <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
+                            <span>Attending Test Student: {{ $item->students_count }}</span>
                         </div>
                     </div>
-                @endif
-           
-            <!-- List Item 1 - END -->
-            @empty
-                
-            @endforelse
-            {{-- <!-- List Item 1 - START -->
-            <div class="col-12 my-2 bg_white-s br_10px-s">
-                <div class="row py-3">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12">
-                        <h5 class="fg-success-s">Landing Page Design</h5>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
-                        <h6>Type: Test <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> 30 Minutes</h6>
-                    </div>
                 </div>
-                <div class="row">
-                    <div class="col-11 fg_dark-s">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s?</p>
-                    </div>
-                </div>
-                <div class="row py-3">
-                    <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
-                        <span>Tottal Students: 40</span>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
-                        <span>Attending Test Student: 40</span>
-                    </div>
-                </div>
-            </div>
-            <!-- List Item 1 - END -->
+            @endif
 
-            <!-- List Item 2 - START -->
-            <div class="col-12 my-2 bg_white-s br_10px-s">
-                <div class="row py-3">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12">
-                        <h5 class="fg-success-s">Landing Page Design</h5>
+            @if ($item->type == 'test')
+                <div class="col-12 my-2 bg_white-s br_10px-s">
+                    <div class="row py-3">
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                            <h5 class="fg-success-s">{{ $item->course->title }}</h5>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
+                            <h6>Type: {{ strtoupper($item->type) }} <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> {{ $item->duration_mins }} Minutes</h6>
+                        </div>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
-                        <h6>Type: Test <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> 30 Minutes</h6>
+                    @foreach ($item->questions as $question)
+                        <div class="row">
+                            <div class="col-11 fg_dark-s">
+                                <p>{{ $question->body }}</p>
+                                @break;
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="row py-3">
+                        <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
+                            <span>Tottal Students: {{ $item->students_count }}</span>
+                        </div>
+                        <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
+                            <span>Attending Test Student: {{ $item->students_count }}</span>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-11 fg_dark-s">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s?</p>
-                    </div>
-                </div>
-                <div class="row py-3">
-                    <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
-                        <span>Tottal Students: 40</span>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
-                        <span>Attending Test Student: 40</span>
-                    </div>
-                </div>
-            </div>
-            <!-- List Item 2 - END -->
+            @endif
 
-            <!-- List Item 3 - START -->
-            <div class="col-12 my-2 bg_white-s br_10px-s">
-                <div class="row py-3">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12">
-                        <h5 class="fg-success-s">Landing Page Design</h5>
+            @if ($item->type == 'boolean')
+                <div class="col-12 my-2 bg_white-s br_10px-s">
+                    <div class="row py-3">
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12">
+                            <h5 class="fg-success-s">{{ $item->course->title }}</h5>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
+                            <h6>Type: {{   ucwords($item->type == 'boolean'? 'true false' : '') }} <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> {{ $item->duration_mins }} Minutes</h6>
+                        </div>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
-                        <h6>Type: Test <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> 30 Minutes</h6>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-11 fg_dark-s">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s?</p>
-                    </div>
-                </div>
-                <div class="row py-3">
-                    <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
-                        <span>Tottal Students: 40</span>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
-                        <span>Attending Test Student: 40</span>
-                    </div>
-                </div>
-            </div>
-            <!-- List Item 3 - END -->
-
-            <!-- List Item 4 - START -->
-            <div class="col-12 my-2 bg_white-s br_10px-s">
-                <div class="row py-3">
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12">
-                        <h5 class="fg-success-s">Landing Page Design</h5>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-12 col-12 fg-success-s text--xl-right">
-                        <h6>Type: Test <span><img class="pl-3" src="assets/preview/clock_icon.svg" alt=""></span> 30 Minutes</h6>
+                    @foreach ($item->questions as $question)
+                        <div class="row">
+                            <div class="col-11 fg_dark-s">
+                                <p>{{ $question->body }}</p>
+                                @break;
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="row py-3">
+                        <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
+                            <span>Tottal Students: {{ $item->students_count }}</span>
+                        </div>
+                        <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
+                            <span>Attending Test Student: {{ $item->students_count }}</span>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-11 fg_dark-s">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s?</p>
-                    </div>
-                </div>
-                <div class="row py-3">
-                    <div class="col-xl-3 col-lg-6 col-md-5 col-12 fg_dark-s">
-                        <span>Tottal Students: 40</span>
-                    </div>
-                    <div class="col-xl-3 col-lg-6 col-md-7 col-12 fg_dark-s">
-                        <span>Attending Test Student: 40</span>
-                    </div>
-                </div>
-            </div>
-            <!-- List Item 4 - END --> --}}
-
-        </div>
-        <!-- Quiz list - END -->
+            @endif
+        @empty
+            
+        @endforelse
+        <!-- List Item 1 - END -->
     </div>
+    <!-- Quiz list - END -->
+</div>
 @endsection
 
 
