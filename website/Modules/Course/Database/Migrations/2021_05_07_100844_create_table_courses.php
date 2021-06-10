fa67261a9067cec077f43a9bd76b3454cbba9771
shams-farooq <shams.kodextech@gmail.com>
2021-06-10 12:21:58 +0500

    commit addition in course migration

--- website/Modules/Course/Database/Migrations/2021_05_07_100844_create_table_courses.php
+++ website/Modules/Course/Database/Migrations/2021_05_07_100844_create_table_courses.php
@@ -20,6 +20,7 @@ class CreateTableCourses extends Migration
             $table->string('title')->nullable();
             $table->enum('nature', ['video', 'online']);
 
+            $table->integer('approver_id')->unsigned()->nullable();
             $table->integer('teacher_id')->unsigned();
             $table->integer('course_category_id')->unsigned();
 
@@ -58,6 +59,9 @@ class CreateTableCourses extends Migration
             $table->index('course_category_id');
             $table->foreign('course_category_id')->references('id')->on('course_categories')->onUpdate('cascade')->onDelete('cascade');
 
+            $table->index('approver_id');
+            $table->foreign('approver_id')->references('id')->on('profiles')->onUpdate('cascade')->onDelete('set null');
+            
             $table->softDeletes();
             $table->timestamps();
         });
