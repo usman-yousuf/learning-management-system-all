b8fbb1464e9cea129c7f69789975971661cd07a4
Ahmed Nawaz Butt <ahmed.n@kodextech.com>
2021-06-22 20:12:24 +0500

    updated fixes

--- website/Modules/Teacher/Resources/views/layouts/nav_links.blade.php
+++ website/Modules/Teacher/Resources/views/layouts/nav_links.blade.php
@@ -1,8 +1,17 @@
 @php
     $pageUrl = $_SERVER['REQUEST_URI'];
+
     $coursesLinks = ['/courses', 'view-course/'];
     $dashboardLinks = ['/dashboard'];
     $studentLinks = ['/student-list'];
+
+    $privacyLinks = ['/privacy'];
+    $aboutLinks = ['/about-us'];
+    $quizLinks = ['/all-quizez', 'view-quiz/'];
+
+    $calendarLinks = ['/activities'];
+    $salesLinks = ['/sales-report'];
+    $reportLinks = ['/general-report'];
 @endphp
 
     <a href="{{ route('teacher.dashboard')}}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($dashboardLinks, $pageUrl) ) active @endif">
@@ -17,19 +26,20 @@
         <img src="{{ asset('assets/images/student_icon.svg') }}" class="ml-3 filter-green-student" width="25" alt="">
         <span class="px-3">Students</span>
     </a>
-    <a href="{{ route('activity.index') }}" class="list-group-item list-group-item-action p-3">
+
+    <a href="{{ route('activity.index') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($calendarLinks, $pageUrl) ) active @endif">
         <img src="{{ asset('assets/images/calendar_icon.svg') }}" class="ml-3" width="25" alt="">
         <span class="px-3">Calendar</span>
     </a>
-    <a href="{{ route('quiz.index') }}" class="list-group-item list-group-item-action p-3">
+    <a href="{{ route('quiz.index') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($quizLinks, $pageUrl) ) active @endif">
         <img src="{{ asset('assets/images/quiz_icon.svg') }}" class="ml-3" width="25" alt="">
         <span class="px-3">Quiz</span>
     </a>
-    <a href="{{ route('report.general') }}" class="list-group-item list-group-item-action p-3">
+    <a href="{{ route('report.general') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($reportLinks, $pageUrl) ) active @endif">
         <img src="{{ asset('assets/images/report_icon.svg') }}" class="ml-3" width="25" alt="">
         <span class="px-3">Report</span>
     </a>
-    <a href="{{ route('report.sales') }}" class="list-group-item list-group-item-action p-3">
+    <a href="{{ route('report.sales') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($salesLinks, $pageUrl) ) active @endif">
         <img src="{{ asset('assets/images/sales-report_icon.svg') }}" class="ml-3" width="25" alt="">
         <span class="px-3">Sales Report</span>
     </a>
@@ -41,12 +51,12 @@
         <img src="{{ asset('assets/images/payment_icon.svg') }}" class="ml-3" width="25" alt="">
         <span class="px-3">Payment</span>
     </a>  --}}
-    <a href="{{ route('cms.privacy-policy') }}" class="list-group-item list-group-item-action p-3">
-        <img src="{{ asset('assets/images/privacy_icon.svg') }}" class="ml-3" width="25" alt="">
+    <a href="{{ route('cms.privacy-policy') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($privacyLinks, $pageUrl) ) active @endif">
+        <img src="{{ asset('assets/images/privacy_icon.svg') }}" class="ml-3" width="25" alt="privacy-icon">
         <span class="px-3">Privacy</span>
     </a>
-    <a href="{{ route('cms.about-us') }}" class="list-group-item list-group-item-action p-3">
-        <img src="{{ asset('assets/images/about_icon.svg') }}" class="ml-3" width="25" alt="">
+    <a href="{{ route('cms.about-us') }}" class="list-group-item list-group-item-action p-3 @if( checkStringAgainstList($aboutLinks, $pageUrl) ) active @endif">
+        <img src="{{ asset('assets/images/about_icon.svg') }}" class="ml-3" width="25" alt="about icon">
         <span class="px-3">About Us</span>
     </a>
     <a href="{{ route('signout') }}" class="list-group-item list-group-item-action p-3 my-5">
