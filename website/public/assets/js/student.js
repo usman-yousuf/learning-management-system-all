0b144b5132b90221d222ee64877d2bbb1b2f4e3a
minarauf <mina.rauf@kodextech.com>
2021-06-23 20:07:23 +0500

    complete changes

--- website/public/assets/js/student.js
+++ website/public/assets/js/student.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-var clickelement = document.getElementsByClassName("search_Dropdown-d");
-Array.from(clickelement).forEach( elm => {
-    elm.addEventListener("click" , showDropMenu);
-});
-
-function showDropMenu() {
-    document.getElementById("search_ref_option-d").classList.toggle("show");
-}
-
+    var clickelement = document.getElementsByClassName("search_dropdown-d");
+    Array.from(clickelement).forEach( elm => {
+        elm.addEventListener("click" , showDropMenu);
+    });
+    
+    function showDropMenu() {
+        document.getElementById("search_ref_option-d").classList.remove("display_none-s");
+        document.getElementById("search_ref_option-d").classList.toggle("display_block-s");
+    }
+    // close dropdown when click anywhere else
+    onclick = function(event) {
+        if (!event.target.matches('.search_dropdown-d')) {
+            var dropdowns = document.getElementsByClassName("search_ref_option-d");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('display_block-s')) {
+                   openDropdown.classList.remove('display_block-s');
+                }
+            }
+        }
+    } 
+    
 });
